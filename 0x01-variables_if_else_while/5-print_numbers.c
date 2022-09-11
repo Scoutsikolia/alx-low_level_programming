ed59adaa0332373751d66f0d48ad740a069991ce
Young Scout <scoutyoung17@gmail.com>
2022-09-11 11:14:43 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,39 @@
+/*
+ *
+ *file 2-print_alphabet.c
+ *
+ */
+
+
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *main - prints the alphabet in lowercase,
+ *
+ *followed by a new line
+ *
+ *
+ *Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+
+	printf("%d", i);
+
+	printf("\n");
+
+
+
+	return (0);
+
+}
